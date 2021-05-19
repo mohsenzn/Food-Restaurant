3966985dce774d0cb6f11ff6615a1735f363fac5
Mohsenzergani <mohsen.zergani@gmail.com>
2021-05-19 20:22:25 +0430

    compelete dish section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,16 @@
 const hamburgerIcon = document.querySelector(".fa-hamburger");
 const nav = document.querySelector("nav");
+const header = document.querySelector(".header")
 hamburgerIcon.addEventListener("click", function () {
   this.classList.toggle("fa-times");
   nav.classList.toggle('nav-toggle')
 });
 
+window.addEventListener('scroll', () => {
+  if(document.body.scrollTop > 10 || document.documentElement.scrollTop > 10 ) {
+    header.classList.add("active-header")
+  } else {
+    header.classList.remove("active-header")
+
+  }
+});
